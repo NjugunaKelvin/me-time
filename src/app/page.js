6cab6d2ec0d7df43d7ce7f50a8f79d17f3139435
NjugunaKelvin <njugunak349@gmail.com>
2025-11-20 14:05:50 +0300

    updated the home page

--- src/app/page.js
+++ src/app/page.js
@@ -5,14 +5,20 @@ export default function Home() {
     <div className=" flex flex-col min-h-screen">
       <Navbar />
       <div className="flex-grow px-32 items-center justify-center">
-        <h1 className="text-4xl font-bold  items-center text-amber-400">Njuguna Kelvin</h1>
-        <p className="py-2 text-xl">I am a Junior Full-Stack Developer and AI/ML enthusiast based in Kenya. I enjoy building clean, efficient, and impactful solutions that solve real-world challenges while delivering great user experiences.
-
-I excel in remote work environments, maintaining clear communication, meeting deadlines, and consistently delivering results. I have a strong curiosity for technology and a drive to stay ahead in the ever-evolving landscape of software development.
-
-Beyond coding, I’m deeply fascinated by artificial intelligence and machine learning, constantly exploring ways they can be applied to everyday problems. I’m also a passionate chess player — a game I value for its strategy, patience, and mental discipline, qualities I bring into my work.
-
-When I’m not in front of a screen, I enjoy reading about emerging technologies, experimenting with side projects, and engaging in activities that challenge my creativity and problem-solving skills.</p>
+        <h1 className="text-4xl font-bold  items-center text-amber-400">Njuguna Kelvin (Vin)</h1>
+        <br/> <br/>
+        <span className="font-extrabold text-white text-xl" >1.0: About Me</span>
+        <p className="py-2 text-xl text-amber-50">
+          Full-Stack Developer and AI/ML practitioner focused on delivering scalable, high-quality solutions that solve complex problems.
+           Experienced in remote collaboration, I combine technical rigor, strategic thinking, and creative problem-solving to drive measurable 
+           impact. Passionate about emerging technologies, I translate AI and software innovations into practical applications while maintaining 
+           a strong commitment to performance, efficiency, and user experience.
+        </p>
+        <br/> <br/>
+        <span className="font-extrabold text-white text-xl">1.1: My Stack</span>
+        <p className="text-amber-50">
+          hello
+        </p>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Navbar() {
     return(
         <nav className="my-6 font-bold text-xl">
-            <ul className="flex items-center justify-center gap-6">
-                <Link className="hover:text-amber-400" href='/'> Home</Link>
-                <Link className="hover:text-amber-400" href='/projects'> Projects</Link>
+            <ul className="flex items-center justify-center text-amber-50 gap-6">
+                <Link className="hover:text-blue-600" href='/'> Home</Link>
+                <Link className="hover:text-blue-600" href='/projects'> Projects</Link>
                 <Link className="hover:text-amber-400" href='/articles'> Articles</Link>
                 <Link className="hover:text-amber-400" href='/contact'> Reach out</Link>
             </ul>
